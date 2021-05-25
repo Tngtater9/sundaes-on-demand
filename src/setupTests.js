1cc9fc414a8f55b931cc777c6edd62cf15329044
Tater <2012849@cognizant.com>
2021-05-25 11:42:56 -0400

    adjusting

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,20 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+// custom-transformer.js
+'use strict';
+
+const {transform} = require('@babel/core');
+const jestPreset = require('babel-preset-jest');
+
+module.exports = {
+  process(src, filename) {
+    const result = transform(src, {
+      filename,
+      presets: [jestPreset],
+    });
+
+    return result || src;
+  },
+};
